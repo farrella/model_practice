e85a118a30097e8c37b0bbf2f86c23006771535c
Abby Farrell <abbykass@gmail.com>
2014-05-11 14:27:25 -0500

    updated q1 - now complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,37 +1,22 @@
 class QuestionsController < ApplicationController
+
+
   def question_1
     # What is the most recent movie on the list that the second actor appeared in?
 
-    the_actor = Actor.second
-
-    the_actors_movies = []
-
-    the_actor.roles.each do |the_role|
-      the_actors_movies.push(the_role.movie)
-    end
-
-    most_recent_movie = { :year => 0, :title => "" }
-
-    the_actors_movies.each do |the_movie|
-      if the_movie.year.to_i > most_recent_movie[:year]
-        most_recent_movie = { :year => the_movie.year.to_i, :title => the_movie.title }
-      end
-    end
-
-    @most_recent_movie_for_second_actor = most_recent_movie[:title]
+    @most_recent_movie_for_second_actor = Actor.find(2).movies.order("year DESC").first.title
 
   end
+
+
 
   def question_2
     # Who directed the longest movie on the list?
 
-    # Your Ruby goes here.
-
-
     @director_of_longest_movie = Movie.order("duration DESC").first.director.name
 
-  
   end
+
 
   def question_3
     # Which director has the most movies on the list?
