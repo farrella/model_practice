ffe3f73436527d0ba12944b4b1dacb997acf6aac
Abby Farrell <abbykass@gmail.com>
2014-05-10 21:15:48 -0500

    q1 done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,25 @@ class QuestionsController < ApplicationController
   def question_1
     # What is the most recent movie on the list that the second actor appeared in?
 
-    # Your Ruby goes here.
+    the_actor = Actor.second
 
-    # @most_recent_movie_for_second_actor = ???
+    the_actors_movies = []
+
+    the_actor.roles.each do |the_role|
+      the_actors_movies.push(the_role.movie)
+    end
+
+    most_recent_movie = { :year => 0, :title => "" }
+
+    the_actors_movies.each do |the_movie|
+      if the_movie.year.to_i > most_recent_movie[:year]
+        most_recent_movie = { :year => the_movie.year.to_i, :title => the_movie.title }
+      end
+    end
+
+    @most_recent_movie_for_second_actor = most_recent_movie[:title]
+
+
   end
 
   def question_2
